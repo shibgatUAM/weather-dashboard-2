42997977393e4a6d5a03619697e19308631687a9
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2026-04-22 09:48:32 +0600

    Weather Dashboard raw template is converted to Components.

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,11 +1,14 @@
 import Header from './components/header/Header';
+import WeatherBoard from './components/weather/WeatherBoard';
 
 export default function Page() {
   return (
     <>
       <Header />
-      <main className="grow grid place-items-center">
-        <section></section>
+      <main className="flex justify-center items-center pt-28 md:pt-36 px-4 pb-12">
+        <section>
+          <WeatherBoard />
+        </section>
       </main>
     </>
   );
---
+++ src/components/weather/WeatherCondition.jsx
@@ -0,0 +1,52 @@
+import CloudImage from '../../assets/icons/cloud.svg';
+import HumadityImage from '../../assets/icons/humidity.svg';
+import TempMaxImage from '../../assets/icons/temp-max.svg';
+import TempMinImage from '../../assets/icons/temp-min.svg';
+import WindImage from '../../assets/icons/wind.svg';
+
+export default function WeatherCondition() {
+  return (
+    <div>
+      <p className="text-sm lg:text-lg font-bold uppercase mb-8">
+        thunderstorm with light drizzle
+      </p>
+      <ul className="space-y-6 lg:space-y-6">
+        <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
+          <span>Temp max</span>
+          <div className="inline-flex space-x-4">
+            <p>19°</p>
+            <img src={TempMaxImage} alt="temp-max" />
+          </div>
+        </li>
+        <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
+          <span>Temp min</span>
+          <div className="inline-flex space-x-4">
+            <p>19°</p>
+            <img src={TempMinImage} alt="temp-min" />
+          </div>
+        </li>
+        <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
+          <span>Humadity</span>
+          <div className="inline-flex space-x-4">
+            <p>58%</p>
+            <img src={HumadityImage} alt="humidity" />
+          </div>
+        </li>
+        <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
+          <span>Cloudy</span>
+          <div className="inline-flex space-x-4">
+            <p>86%</p>
+            <img src={CloudImage} alt="cloudy" />
+          </div>
+        </li>
+        <li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
+          <span>Wind</span>
+          <div className="inline-flex space-x-4">
+            <p>5km/h</p>
+            <img src={WindImage} alt="wind" />
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+}
