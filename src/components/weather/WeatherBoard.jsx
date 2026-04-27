57fbbdddc001beede27182cff82133fda3686252
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2026-04-27 09:35:21 +0600

    Created useWeather hook and fetching data from hook.

--- src/components/weather/WeatherBoard.jsx
+++ src/components/weather/WeatherBoard.jsx
@@ -1,8 +1,13 @@
+import { useWeather } from '../../hooks';
 import AddToFavourite from './AddToFavourite';
 import WeatherCondition from './WeatherCondition';
 import WeatherHeadline from './WeatherHeadline';
 
 export default function WeatherBoard() {
+  const { weatherData, error, loading } = useWeather();
+
+  console.log(weatherData, error, loading);
+
   return (
     <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/14 px-4 lg:px-14 py-6 lg:py-10 min-h-130 max-w-264.5 mx-auto">
       <div className="grid md:grid-cols-2 gap-10 md:gap-6">
