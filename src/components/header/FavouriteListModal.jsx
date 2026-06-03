a00056b9aa002698b09a6597b15549318eba9c47
Shibgat Ullah Al Mahmud <shibgat@hotmail.com>
2026-06-03 10:57:58 +0600

    Implemented Favourite Modal functionality

--- src/components/header/FavouriteListModal.jsx
+++ src/components/header/FavouriteListModal.jsx
@@ -1,11 +1,27 @@
+import { useFavouriteContext } from '../../context';
+
 export default function FavouriteListModal() {
+  const { favourites } = useFavouriteContext();
+
   return (
-    <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
+    <div className="max-w-xs py-4 bg-white rounded-md border border-gray-200 absolute right-0 top-16 text-black shadow-lg z-50">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
+
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-        <li className="hover:bg-gray-200">Dhaka</li>
-        <li className="hover:bg-gray-200">Rangpur</li>
-        <li className="hover:bg-gray-200">Europe</li>
+        {favourites.length > 0 ? (
+          favourites.map((fav) => (
+            <li
+              key={fav.location}
+              className="hover:bg-gray-100 transition-colors"
+            >
+              {fav.location}
+            </li>
+          ))
+        ) : (
+          <li className="text-gray-500 text-sm cursor-default">
+            Nothing is added
+          </li>
+        )}
       </ul>
     </div>
   );
